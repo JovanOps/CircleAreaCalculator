ddbb6b61afe5586bb26a53eaca16f436d1503a3d
Korisnik <Korisnik@DESKTOP-SGVK0U9>
2025-07-06 00:57:43 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PovrsinaKruga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Func<double, string> povrsinaKruga = r =>
+            {
+                double povrsina = Math.PI * r * r;
+                return $"Površina kruga poluprečnika {r} je {povrsina:F2}.";
+            };
+
+            
+            Console.WriteLine("Unesite poluprečnik kruga:");
+            if (double.TryParse(Console.ReadLine(), out double poluprecnik))
+            {
+                string rezultat = povrsinaKruga(poluprecnik);
+                Console.WriteLine(rezultat);
+            }
+            else
+            {
+                Console.WriteLine("Unet je nevažeći broj.");
+            }
+
+            Console.WriteLine("Pritisnite bilo koji taster za izlaz...");
+            Console.ReadKey();
+        }
+    }
+}
+
